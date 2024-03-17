53aad242ef25fc4a33027e2ac575ef9b2795f3bf
Elizabeth Ramos <elizabeth.ramos@udgvirtual.udg.mx>
2024-03-17 13:59:45 -0600

    Prepaf bala creado

--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -10,18 +10,27 @@ public class JohnMovement : MonoBehaviour
     public bool Grounded;
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        // Direction of John
+        if(Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f,1f,1f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f,1f,1f);
+
+        // Animation
+        Animator.SetBool("running",Horizontal != 0.0f);
 
         Debug.DrawRay(transform.position,Vector3.down *0.1f,Color.red);
         if(Physics2D.Raycast(transform.position, Vector3.down,0.1f))
