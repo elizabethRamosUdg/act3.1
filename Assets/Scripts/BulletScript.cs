c9c3993bfd00a70a2d107cfb66219366a2861f76
Elizabeth Ramos <elizabeth.ramos@udgvirtual.udg.mx>
2024-03-17 14:17:55 -0600

    Camara sigue al jugador

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletScript : MonoBehaviour
-{   
+{
     public float Speed;
+    private Vector2 Direction;
     private Rigidbody2D Rigidbody2D;
     // Start is called before the first frame update
     void Start()
@@ -15,6 +14,16 @@ public class BulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rigidbody2D.velocity = Vector2.right * Speed;
+        Rigidbody2D.velocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        Direction = direction;
+    }
+
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/JohnMovement.cs
+++ Assets/Scripts/JohnMovement.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class JohnMovement : MonoBehaviour
 {
+    public GameObject BulletPrefab;
     public float JumpForce;
     public float Speed;
     public bool Grounded;
@@ -12,7 +10,8 @@ public class JohnMovement : MonoBehaviour
     private Rigidbody2D Rigidbody2D;
     private Animator Animator;
     private float Horizontal;
-
+    // Time before last shoot
+    private float LastShoot;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,29 +25,52 @@ public class JohnMovement : MonoBehaviour
         Horizontal = Input.GetAxisRaw("Horizontal");
 
         // Direction of John
-        if(Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f,1f,1f);
-        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f,1f,1f);
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f, 1f, 1f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f, 1f, 1f);
 
         // Animation
-        Animator.SetBool("running",Horizontal != 0.0f);
+        Animator.SetBool("running", Horizontal != 0.0f);
 
-        Debug.DrawRay(transform.position,Vector3.down *0.1f,Color.red);
-        if(Physics2D.Raycast(transform.position, Vector3.down,0.1f))
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
         {
             Grounded = true;
         }
         else Grounded = false;
 
+        // Jump (W)
         if (Input.GetKeyDown(KeyCode.W) && Grounded)
         {
             Jump();
         }
+
+        // Shoot (Space)
+        if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            // Last shoot
+            LastShoot = Time.time;
+        }
+        
     }
 
     // Jump 
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    // Shoot 
+    private void Shoot()
+    {   
+        Vector3 direction;
+
+        // Right
+        if(transform.localScale.x == 1) direction = Vector3.right;
+        else direction = Vector3.left;
+
+        GameObject bullet = Instantiate(BulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().SetDirection(direction);
     }
 
     // Update for items with Physics
